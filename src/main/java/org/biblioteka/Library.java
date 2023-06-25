ba3ee05034e077876fd7e2a3f15ac3f1280ceaa1
Piotr Smoła <piotrsmola2002@gmail.com>
2023-06-25 11:15:20 +0200

    update Library.java

--- src/main/java/org/biblioteka/Library.java
+++ src/main/java/org/biblioteka/Library.java
@@ -1,4 +1,19 @@
 package org.biblioteka;
 
-public class Library {
+import java.util.Scanner;
+
+//zgodnie ze standardami nazewnictwa nazwy klas, interfejsów oraz zmiennych są w języku angielskim, zaś komunikaty i opcje menu dla lepszego odbioru są w języku polskim
+class Library {
+    private BookDatabase bookDatabase;
+    private ReaderDatabase readerDatabase;
+    private Scanner in;
+
+    public Library(BookDatabase bookDatabase, ReaderDatabase readerDatabase) {
+        this.bookDatabase = bookDatabase;
+        this.readerDatabase = readerDatabase;
+        this.in = new Scanner(System.in);
+    }
+
+
+
 }
