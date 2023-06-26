8c8701cc60b823bdc60a0de2612308182c9fc979
Piotr Smoła <piotrsmola2002@gmail.com>
2023-06-26 18:12:30 +0200

    update Library.java and BookDatabase.java

--- src/main/java/org/biblioteka/BookDatabase.java
+++ src/main/java/org/biblioteka/BookDatabase.java
@@ -1,4 +1,170 @@
 package org.biblioteka;
 
-public class BookDatabase {
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+import com.opencsv.exceptions.CsvValidationException;
+import java.time.LocalDate;
+import java.util.NoSuchElementException;
+import java.util.UUID;
+
+class BookDatabase implements DatabaseOperations, CSVOperations {
+    private List<Book> books;
+    private List<BorrowInfo> borrowInfoList;
+
+    public BookDatabase() {
+        this.books = new ArrayList<>();
+        this.borrowInfoList = new ArrayList<>();
+        try {
+            loadFromDatabase("A:\\baza do projektu\\books.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addBook(Book book) throws IllegalArgumentException {
+        if (findBookByTitleAndAuthor(book.getTitle(), book.getAuthor()) != null) {
+            throw new IllegalArgumentException("The book is already in the database.");
+        }
+        books.add(book);
+        try {
+            saveToDatabase("A:\\baza do projektu\\books.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void removeBook(Book book) throws NoSuchElementException {
+        if (!books.remove(book)) {
+            throw new NoSuchElementException("No such book in the database.");
+        }
+        try {
+            saveToDatabase("books.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public Book findBookByTitleAndAuthor(String title, String author) {
+        for (Book book : books) {
+            if (book.getTitle().equalsIgnoreCase(title) && book.getAuthor().equalsIgnoreCase(author)) {
+                return book;
+            }
+        }
+        return null;
+    }
+
+    public BorrowInfo borrowBook(String title, String author, String readerId) {
+        if (title == null || title.isBlank() || author == null || author.isBlank() || readerId == null || readerId.isBlank()) {
+            throw new IllegalArgumentException("Invalid input. Title, author and readerId cannot be empty.");
+        }
+        Book book = findBookByTitleAndAuthor(title, author);
+        UUID readerID = UUID.fromString(readerId);
+        if (book != null && book.isAvailable()) {
+            book.setNumberOfCopies(book.getNumberOfCopies() - 1);
+            BorrowInfo borrowInfo = new BorrowInfo(book, readerID, LocalDate.now());
+            borrowInfoList.add(borrowInfo);
+            System.out.println("Book borrowed successfully: " + book);
+            return borrowInfo;
+        } else {
+            System.out.println("Book not available for borrowing.");
+            return null;
+        }
+    }
+
+    public BorrowInfo returnBook(String title, String author, String readerId) {
+        if (title == null || title.isBlank() || author == null || author.isBlank() || readerId == null || readerId.isBlank()) {
+            throw new IllegalArgumentException("Invalid input. Title, author and readerId cannot be empty.");
+        }
+        Book book = findBookByTitleAndAuthor(title, author);
+        BorrowInfo borrowInfo = findBorrowInfo(title, author, readerId);
+        if (book != null && borrowInfo != null) {
+            book.setNumberOfCopies(book.getNumberOfCopies() + 1);
+            borrowInfoList.remove(borrowInfo);
+            System.out.println("Book returned successfully: " + book);
+            return borrowInfo;
+        } else {
+            System.out.println("Invalid book title, reader ID, or book not borrowed by this reader.");
+            return null;
+        }
+    }
+
+    private BorrowInfo findBorrowInfo(String title, String author, String readerId) {
+        for (BorrowInfo borrowInfo : borrowInfoList) {
+            if (borrowInfo.getBook().getTitle().equals(title) && borrowInfo.getBook().getAuthor().equals(author) && borrowInfo.getReaderId().equals(readerId)) {
+                return borrowInfo;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void saveToDatabase(String filePath) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            for (Book book : books) {
+                writer.write(book.getTitle() + "," + book.getAuthor() + "," + book.getNumberOfCopies() + "\n");
+            }
+        }
+    }
+
+    @Override
+    public void loadFromDatabase(String filePath) throws IOException {
+        books.clear();
+        File file = new File(filePath);
+        if (file.exists()) {
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    String[] parts = line.split(",");
+                    String title = parts[0];
+                    String author = parts[1];
+                    int numberOfCopies = Integer.parseInt(parts[2]);
+                    books.add(new RegularBook(title, author, numberOfCopies));
+                }
+            }
+        } else {
+            System.out.println("File " + filePath + " does not exist. No data loaded.");
+        }
+    }
+
+
+    @Override
+    public void importFromCSV(String filePath) {
+        try (CSVReader reader = new CSVReader(new FileReader(filePath))) {
+            String[] line;
+            while ((line = reader.readNext()) != null) {
+                String title = line[0];
+                String author = line[1];
+                int copies = Integer.parseInt(line[2]);
+                this.books.add(new RegularBook(title, author, copies));
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred while reading the file.");
+            e.printStackTrace();
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid data format in the file.");
+            e.printStackTrace();
+        } catch (CsvValidationException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void exportToCSV(String filePath) {
+        try (CSVWriter writer = new CSVWriter(new FileWriter(filePath, true))) {
+            for (Book book : books) {
+                String[] line = {book.getTitle(), book.getAuthor(), Integer.toString(book.getNumberOfCopies())};
+                writer.writeNext(line);
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred while writing to the file.");
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/org/biblioteka/Library.java
+++ src/main/java/org/biblioteka/Library.java
@@ -1,6 +1,8 @@
 package org.biblioteka;
-
-import java.util.Scanner;
+import java.io.*;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.*;
 
 //zgodnie ze standardami nazewnictwa nazwy klas, interfejsów oraz zmiennych są w języku angielskim, zaś komunikaty i opcje menu dla lepszego odbioru są w języku polskim
 class Library {
@@ -36,30 +38,37 @@ class Library {
             switch (choice) {
                 case "1":
                     addBook();
+                    saveState();
                     break;
                 case "2":
                     borrowBook();
+                    saveState();
                     break;
                 case "3":
                     returnBook();
+                    saveState();
                     break;
                 case "4":
                     searchBook();
                     break;
                 case "5":
                     addReader();
+                    saveState();
                     break;
                 case "6":
                     removeReader();
+                    saveState();
                     break;
                 case "7":
                     importBooksFromCSV();
+                    saveState();
                     break;
                 case "8":
                     exportBooksToCSV();
                     break;
                 case "9":
                     importReadersFromCSV();
+                    saveState();
                     break;
                 case "10":
                     exportReadersToCSV();
@@ -68,9 +77,230 @@ class Library {
                     exit = true;
                     break;
                 default:
-                    System.out.println("Niewłaściwa opcja. Spróbuj ponownie");
+                    System.out.println("Invalid option. Please try again.");
                     break;
             }
         }
     }
+
+    public void borrowBook(UUID readerId, Book book) throws IOException {
+        if (book.isAvailable()) {
+            book.setNumberOfCopies(book.getNumberOfCopies() - 1);
+            borrows.add(new BorrowInfo(book, readerId, LocalDate.now()));
+            saveBorrowsToDatabase("A:\\baza do projektu\\borrows.txt");
+        } else {
+            System.out.println("The book is not available.");
+        }
+    }
+
+    public void returnBook(UUID readerId, Book book) throws IOException {
+        BorrowInfo borrowInfo = borrows.stream()
+                .filter(borrow -> borrow.getBook().equals(book) && borrow.getReaderId().equals(readerId))
+                .findFirst()
+                .orElse(null);
+        if (borrowInfo != null) {
+            book.setNumberOfCopies(book.getNumberOfCopies() + 1);
+            borrows.remove(borrowInfo);
+            saveBorrowsToDatabase("A:\\baza do projektu\\borrows.txt");
+        } else {
+            System.out.println("No such borrow found.");
+        }
+    }
+
+    private void saveBorrowsToDatabase(String filePath) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            for (BorrowInfo borrow : borrows) {
+                writer.write(borrow.getReaderId() + "," + borrow.getBook().getTitle() + "," + borrow.getBook().getAuthor() + "," + borrow.getBorrowDate() + "\n");
+            }
+        }
+    }
+
+    private void loadBorrowsFromDatabase(String filePath) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                UUID readerId = UUID.fromString(parts[0]);
+                String title = parts[1];
+                String author = parts[2];
+                Book book = new RegularBook(title, author, 1); // you can replace this with a proper way to find the book
+                LocalDate borrowDate = LocalDate.parse(parts[3]);
+                borrows.add(new BorrowInfo(book, readerId, borrowDate));
+            }
+        }
+    }
+
+    public void importBooksFromCSV() {
+        System.out.println("Enter CSV file path:");
+        String filePath = scanner.nextLine();
+        File file = new File(filePath);
+        if (file.exists() && !file.isDirectory()) {
+            bookDatabase.importFromCSV(filePath);
+        } else {
+            System.out.println("File not found. Please check the path and try again.");
+        }
+    }
+
+    public void exportBooksToCSV() {
+        System.out.println("Enter CSV file path:");
+        String filePath = scanner.nextLine();
+        // W tym przypadku, jeżeli plik nie istnieje, zostanie stworzony.
+        // Jeżeli jednak istnieje, dane zostaną do niego dopisane, więc warto o tym poinformować użytkownika.
+        System.out.println("If the file already exists, new data will be appended to it.");
+        bookDatabase.exportToCSV(filePath);
+    }
+
+    public void importReadersFromCSV() {
+        System.out.println("Enter CSV file path:");
+        String filePath = scanner.nextLine();
+        File file = new File(filePath);
+        if (file.exists() && !file.isDirectory()) {
+            readerDatabase.importFromCSV(filePath);
+        } else {
+            System.out.println("File not found. Please check the path and try again.");
+        }
+    }
+
+    public void exportReadersToCSV() {
+        System.out.println("Enter CSV file path:");
+        String filePath = scanner.nextLine();
+        System.out.println("If the file already exists, new data will be appended to it.");
+        readerDatabase.exportToCSV(filePath);
+    }
+
+    private void borrowBook() {
+        System.out.println("Enter reader ID:");
+        String readerId = scanner.nextLine();
+        System.out.println("Enter book title:");
+        String title = scanner.nextLine();
+        System.out.println("Enter book author:");
+        String author = scanner.nextLine();
+        bookDatabase.borrowBook(title, author, readerId);
+    }
+
+
+
+    private void addBook() {
+        System.out.println("Enter book title:");
+        String title = scanner.nextLine();
+        System.out.println("Enter book author:");
+        String author = scanner.nextLine();
+        System.out.println("Enter number of copies:");
+        int copies = scanner.nextInt();
+        scanner.nextLine(); // Consume newline left-over
+        bookDatabase.addBook(new RegularBook(title, author, copies));
+    }
+
+
+    private void returnBook() {
+        System.out.println("Enter reader ID:");
+        String readerId = scanner.nextLine();
+        System.out.println("Enter book title:");
+        String title = scanner.nextLine();
+        System.out.println("Enter book author:");
+        String author = scanner.nextLine();
+        // Obliczanie kary za opóźnienie
+        BorrowInfo borrowInfo = bookDatabase.returnBook(title, author, readerId);
+        if (borrowInfo != null) {
+            long daysBetween = ChronoUnit.DAYS.between(borrowInfo.getBorrowDate(), LocalDate.now());
+            if (daysBetween > 14) {
+                double fine = (daysBetween - 14) * 1.0; // 1.0 PLN per day fine
+                System.out.println("Book returned successfully. Fine for late return: " + fine + " PLN");
+            } else {
+                System.out.println("Book returned successfully.");
+            }
+        } else {
+            System.out.println("Invalid book title, reader ID, or book not borrowed by this reader.");
+        }
+    }
+
+    private void searchBook() {
+        System.out.println("Enter book title:");
+        String title = scanner.nextLine();
+        System.out.println("Enter book author:");
+        String author = scanner.nextLine();
+        Book book = bookDatabase.findBookByTitleAndAuthor(title, author);
+        if (book != null) {
+            System.out.println("Book found: " + book);
+        } else {
+            System.out.println("Book not found.");
+        }
+    }
+
+    private void addReader() {
+        System.out.println("Enter reader name:");
+        String name = scanner.nextLine();
+        System.out.println("Enter reader PESEL:");
+        String pesel = scanner.nextLine();
+        try {
+            readerDatabase.addReader(new Reader(name, pesel));
+        } catch (IllegalArgumentException e) {
+            System.out.println("Invalid PESEL number. Please try again.");
+        }
+    }
+
+
+    private void removeReader() {
+        System.out.println("Enter reader name:");
+        String name = scanner.nextLine();
+        System.out.println("Enter reader PESEL:");
+        String pesel = scanner.nextLine();
+        try {
+            readerDatabase.removeReader(new Reader(name, pesel));
+        } catch (NoSuchElementException e) {
+            System.out.println("No such reader in the database.");
+        }
+    }
+
+
+    private void loadBorrowInfo() {
+        try (BufferedReader reader = new BufferedReader(new FileReader("A:\\baza do projektu\\borrows.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                String readerId = parts[0];
+                String bookTitle = parts[1];
+                String bookAuthor = parts[2];
+                UUID uid = UUID.fromString(readerId);
+                LocalDate borrowDate = LocalDate.parse(parts[3]);
+                Book book = bookDatabase.findBookByTitleAndAuthor(bookTitle, bookAuthor);
+                if (book != null) {
+                    borrows.add(new BorrowInfo(book, uid, borrowDate));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void saveBorrowInfo() {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("borrows.txt"))) {
+            for (BorrowInfo borrowInfo : borrows) {
+                writer.write(borrowInfo.getBook().getTitle() + ","
+                        + borrowInfo.getBook().getAuthor() + ","
+                        + borrowInfo.getReaderId() + ","
+                        + borrowInfo.getBorrowDate() + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void saveState() {
+        try {
+            this.bookDatabase.saveToDatabase("books.txt");
+            this.readerDatabase.saveToDatabase("readers.txt");
+            this.saveBorrowInfo();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        BookDatabase bookDatabase = new BookDatabase();
+        ReaderDatabase readerDatabase = new ReaderDatabase();
+        Library library = new Library(bookDatabase, readerDatabase);
+        library.startLibraryOperations();
+    }
 }
+
