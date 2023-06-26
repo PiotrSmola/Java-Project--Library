b41fca219f456d5e070abbdf805ad0c8b942b913
Piotr Smoła <piotrsmola2002@gmail.com>
2023-06-26 19:39:35 +0200

    update BorrowInfo.java

--- src/main/java/org/biblioteka/BorrowInfo.java
+++ src/main/java/org/biblioteka/BorrowInfo.java
@@ -1,4 +1,29 @@
 package org.biblioteka;
 
-public class BorrowInfo {
+import java.time.LocalDate;
+import java.util.UUID;
+
+class BorrowInfo {
+    private Book book;
+    private UUID readerId;
+    private LocalDate borrowDate;
+
+    public BorrowInfo(Book book, UUID readerId, LocalDate borrowDate) {
+        this.book = book;
+        this.readerId = readerId;
+        this.borrowDate = borrowDate;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public UUID getReaderId() {
+        return readerId;
+    }
+
+    public LocalDate getBorrowDate() {
+        return borrowDate;
+    }
 }
+
