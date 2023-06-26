a5517365cf765496d44a23896e9abc92e2bccdac
Piotr Smoła <piotrsmola2002@gmail.com>
2023-06-26 19:32:58 +0200

    update DatabaseOperations.java

--- src/main/java/org/biblioteka/DatabaseOperations.java
+++ src/main/java/org/biblioteka/DatabaseOperations.java
@@ -1,4 +1,11 @@
 package org.biblioteka;
 
-public interface DatabaseOperations {
+import java.io.IOException;
+
+// Interfejs do operacji na bazie danych
+interface DatabaseOperations {
+    void saveToDatabase(String filePath) throws IOException;
+    void loadFromDatabase(String filePath) throws IOException;
 }
+
+
